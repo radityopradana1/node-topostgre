f88b0a2b3eb99ca44b05737bc7cea53ac36a9623
radityopradana1 <radityopradana1@gmail.com>
2025-03-11 10:29:06 +0700

    edit yml2

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(function (req, res, next) {
 });
 
 app.get("/", (req, res) => {
-    res.send("Hello World! v1.0");
+    res.send("Hello World! v1.1");
 });
 
 app.use("/api/v1/users", usersRoutes);
