60da1df2efc9f736646815ad749a3f4d8e312f19
radityopradana1 <radityopradana1@gmail.com>
2025-03-12 13:20:29 +0700

    v1.6

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(function (req, res, next) {
 });
 
 app.get("/", (req, res) => {
-    res.send("Hello World! v1.5");
+    res.send("Hello World! v1.6");
 });
 
 app.use("/api/v1/users", usersRoutes);
